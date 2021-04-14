a82d363a43aa54dcf9b1f9885c38d0a21263bc1f
DESKTOP-TD2NHFL\geosoft205 <silvermk3@geosoft.co.kr>
2021-04-14 17:07:17 +0900

    feat: Add  sever/swagger/jpa  configuration, test controller

---
+++ src/test/java/com/toyseven/ymk/ToysevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyseven.ymk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
