9832af95f55f8024ea7fc02c751e75d40ff10267
DESKTOP-TD2NHFL\geosoft205 <silvermk3@geosoft.co.kr>
2021-04-30 10:54:13 +0900

    feat: Configuration Jwt Token

---
+++ src/main/java/com/toyseven/ymk/jwtToken/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.toyseven.ymk.jwtToken;
+
+public class JwtResponse {
+    private final String jwttoken;
+
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+}
