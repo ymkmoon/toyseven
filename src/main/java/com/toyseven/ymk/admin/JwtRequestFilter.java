119dee4e6220efaa53aa00b72e45ecb4bb7626eb
ymkmoon <ymkmoon@naver.com>
2021-10-25 16:59:37 +0900

    Add exception case on jwt request filter

--- src/main/java/com/toyseven/ymk/admin/JwtRequestFilter.java
+++ src/main/java/com/toyseven/ymk/admin/JwtRequestFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,34 +19,40 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import com.toyseven.ymk.common.util.JwtUtil;
 
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureException;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class JwtRequestFilter extends OncePerRequestFilter {
+	
 
-    private final AdminService adminService;
+	private final AdminService adminService;
     private final JwtUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
         throws ServletException, IOException {
+
         String accessToken = getAccessTokenFromRequestHeader(request);
         String username = null;
-
+        
         if(accessToken != null) {
         	try {
         		username = jwtUtil.getUsernameFromToken(accessToken);
-        	} catch (IllegalArgumentException e) {
+        	} catch (IllegalArgumentException | AccessDeniedException | MalformedJwtException | SignatureException e) {
         		logger.info("Unable to get JWT Token");
         		response.sendError(401, "Unable to get JWT Token");
+        		return;
         	} catch (ExpiredJwtException e) {
         		logger.info("JWT Token has expired");
         		response.sendError(401, "JWT Token has expired");
-        	} catch (SignatureException e) {
-        		logger.info("JWT signature does not match locally computed signature");
-        		response.sendError(401, "Unable to get JWT Token");
+        		return;
+        	} catch (Exception e) {
+        		logger.info("Unable to get JWT Token");
+        		response.sendError(500, "Unable to get JWT Token");
+        		return;
         	}
         	
         	if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
