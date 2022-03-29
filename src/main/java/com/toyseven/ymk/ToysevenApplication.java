0840be3d876adf743776060a73daf023c0be7985
ymkmoon <ymkmoon@naver.com>
2022-03-29 14:53:16 +0900

    Delete never used import

--- src/main/java/com/toyseven/ymk/ToysevenApplication.java
+++ src/main/java/com/toyseven/ymk/ToysevenApplication.java
@@ -2,7 +2,6 @@ package com.toyseven.ymk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class ToysevenApplication {
