39c1d68bb12cf98f630468fe901f91892a6eaf11
ymkmoon <ymkmoon@naver.com>
2022-04-01 11:49:52 +0900

    Update to return json object

--- src/main/java/com/toyseven/ymk/aop/FilterChainProxyAdvice.java
+++ src/main/java/com/toyseven/ymk/aop/FilterChainProxyAdvice.java
@@ -1,24 +1,51 @@
 package com.toyseven.ymk.aop;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.MediaType;
 import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.toyseven.ymk.common.error.ErrorCode;
+import com.toyseven.ymk.common.error.ErrorResponse;
+
+import lombok.RequiredArgsConstructor;
+
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class FilterChainProxyAdvice {
+	
+	private final ObjectMapper objectMapper;
 
+	/**
+	 * 요청 시 URI 에 슬래시(/)가 두개 존재 할 경우 실행되는 aop
+	 */
 	@Around("execution(public void org.springframework.security.web.FilterChainProxy.doFilter(..))")
 	public void handleRequestRejectedException (ProceedingJoinPoint pjp) throws Throwable {
 	    try {
 	        pjp.proceed();
 	    } catch (RequestRejectedException exception) {
 	        HttpServletResponse response = (HttpServletResponse) pjp.getArgs()[1];
-	        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	        ErrorResponse fail = ErrorResponse.builder()
+					.status(ErrorCode.BAD_REQUEST.getHttpStatus().value())
+					.error(ErrorCode.BAD_REQUEST.getHttpStatus().name())
+	                .code(ErrorCode.BAD_REQUEST.name())
+	                .message(ErrorCode.BAD_REQUEST.getDetail())
+	                .build();
+			
+		    response.setStatus(ErrorCode.BAD_REQUEST.getHttpStatus().value());
+		    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		    String json = objectMapper.writeValueAsString(fail);
+		    PrintWriter writer = response.getWriter();
+		    writer.write(json);
+		    writer.flush();
 	    }
 	}
 }
