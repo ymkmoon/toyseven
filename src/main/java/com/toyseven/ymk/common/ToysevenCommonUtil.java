fd191b7b0a40d54620e508dfb331d5618d923e3a
ymkmoon <ymkmoon@naver.com>
2022-04-15 16:12:36 +0900

    Add station json file for save data when station api closed

--- src/main/java/com/toyseven/ymk/common/ToysevenCommonUtil.java
+++ src/main/java/com/toyseven/ymk/common/ToysevenCommonUtil.java
@@ -1,6 +1,20 @@
 package com.toyseven.ymk.common;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.toyseven.ymk.common.model.entity.StationInformationEntity;
 
 import lombok.experimental.UtilityClass;
 
@@ -14,5 +28,26 @@ public class ToysevenCommonUtil {
 	 */
 	public static boolean isSuccessResponse(ResponseEntity<?> response) {
 		return response != null && response.getStatusCode().is2xxSuccessful();
+	}
+	
+	private static final Path CURRENT_PATH = Paths.get("src/main/java/com/toyseven/ymk/station");
+
+	public static List<StationInformationEntity> getStationsFromJsonArrayFile(String filePath) {
+		JSONArray stationArray = (JSONArray)readJsonFile(filePath);
+		try {
+			return new ObjectMapper().readValue(stationArray.toString(), new TypeReference<List<StationInformationEntity>>() {});
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public static Object readJsonFile(String filePath) {
+		try {
+			JSONParser parser = new JSONParser();
+			Path p = Paths.get(CURRENT_PATH.toString(), filePath);
+			return parser.parse(new FileReader(p.toString()));
+		} catch (IOException | ParseException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
