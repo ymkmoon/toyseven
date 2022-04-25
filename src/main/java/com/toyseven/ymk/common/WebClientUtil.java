4742bbc1a7a4822ac1213b79dc94951b83bb1422
ymkmoon <ymkmoon@naver.com>
2022-04-25 13:24:47 +0900

    Add uitl class and use it

---
+++ src/main/java/com/toyseven/ymk/common/WebClientUtil.java
@@ -0,0 +1,18 @@
+package com.toyseven.ymk.common;
+
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory.EncodingMode;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class WebClientUtil {
+	
+	public static WebClient buildWebClient(String baseUrl, EncodingMode encodingMode) {
+		DefaultUriBuilderFactory factory = new DefaultUriBuilderFactory(baseUrl);
+        factory.setEncodingMode(encodingMode);
+        return WebClient.builder().uriBuilderFactory(factory).baseUrl(baseUrl).build();
+	}
+	
+}
