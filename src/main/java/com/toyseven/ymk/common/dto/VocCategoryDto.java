e1a3188d7eba2c8225da447028ac46fab2f1f6d1
ymkmoon <ymkmoon@naver.com>
2022-05-04 16:18:56 +0900

    refactor: Delete fields to match the entity

--- src/main/java/com/toyseven/ymk/common/dto/VocCategoryDto.java
+++ src/main/java/com/toyseven/ymk/common/dto/VocCategoryDto.java
@@ -1,11 +1,6 @@
 package com.toyseven.ymk.common.dto;
 
-import java.time.LocalDateTime;
-
 import javax.validation.constraints.NotBlank;
-
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,13 +15,6 @@ public class VocCategoryDto {
 		private Long id;
 		@NotBlank private String name;
 		@NotBlank private String displayName;
-		@JsonDeserialize(using = CustomLocalDateTimeDeserializer.class) 
-		@JsonSerialize(using = CustomLocalDateTimeSerializer.class) 
-		private LocalDateTime createdAt;
-		
-		@JsonDeserialize(using = CustomLocalDateTimeDeserializer.class) 
-		@JsonSerialize(using = CustomLocalDateTimeSerializer.class) 
-		private LocalDateTime updatedAt;
 	}
 	
 }
