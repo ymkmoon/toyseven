ff883616c04610808cefe88f99472132d94035a8
ymkmoon <ymkmoon@naver.com>
2022-06-14 12:57:40 +0900

    Delete optional field declaration

--- src/main/java/com/toyseven/ymk/station/StationRepository.java
+++ src/main/java/com/toyseven/ymk/station/StationRepository.java
@@ -1,7 +1,6 @@
 package com.toyseven.ymk.station;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,6 +10,5 @@ import com.toyseven.ymk.common.model.entity.StationInformationEntity;
 @Repository
 public interface StationRepository extends JpaRepository<StationInformationEntity, Long> {
 	List<StationInformationEntity> findByStationNameContaining(String name);
-
-	Optional<StationInformationEntity> findByStationId(String stationId);
+	StationInformationEntity findByStationId(String stationId);
 }
