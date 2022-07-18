3bb13bb24a5a2c6bca3cd2e55fb53fc3226621bd
ymkmoon <ymkmoon@naver.com>
2022-07-18 17:41:47 +0900

    Add pointcut

--- src/main/java/com/toyseven/ymk/aop/LogAspect.java
+++ src/main/java/com/toyseven/ymk/aop/LogAspect.java
@@ -1,8 +1,11 @@
 package com.toyseven.ymk.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
@@ -22,12 +25,8 @@ public class LogAspect {
 //    @Around("execution(* ~~~.~~~.~~~.~~~..*Service.*(..))")
     @Around("execution(* com.toyseven.ymk..*Service.*(..))")
     public Object logging(ProceedingJoinPoint pjp) throws Throwable {
-        logger.info("start - " + pjp.getSignature().getDeclaringTypeName() + " / " + pjp.getSignature().getName());
-        Object result = pjp.proceed();
-        logger.info("finished - " + pjp.getSignature().getDeclaringTypeName() + " / " + pjp.getSignature().getName());
-        return result;
+    	return around(pjp);
     }
-    
     // 특정 메소드에서 로그 출력
     @Around("execution(* com.toyseven.ymk.voc.VocController.saveVocAnswer(..))")
     public Object loggingWhenSaveVocAnswer(ProceedingJoinPoint pjp) throws Throwable {
@@ -38,5 +37,60 @@ public class LogAspect {
     	return result;
     }
     
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void getMapping() {
+    	logger.info("Pointcut GetMapping(): begin");
+    	logger.info("Pointcut GetMapping(): end");
+    }
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+	public void postMapping() {
+		logger.info("Pointcut PostMapping(): begin");
+		logger.info("Pointcut PostMapping(): end");
+	}
+	@Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
+	public void requestMapping() {
+		logger.info("Pointcut RequestMapping(): begin");
+		logger.info("Pointcut RequestMapping(): end");
+	}
+    
+	
+	@Before("getMapping()") 
+	public void beforeGetMethod(JoinPoint pjp) { 
+		before(pjp);
+	}
+	@Before("postMapping()") 
+	public void beforePostMethod(JoinPoint pjp) { 
+		before(pjp);
+	}
+	@Before("requestMapping()") 
+	public void beforeRequestMethod(JoinPoint pjp) { 
+		before(pjp);
+	}
+	
+
+	@Around("getMapping()")
+	public Object aroundGet(ProceedingJoinPoint pjp) throws Throwable {
+		return around(pjp);
+	}
+	@Around("postMapping()")
+	public Object aroundPost(ProceedingJoinPoint pjp) throws Throwable {
+		return around(pjp);
+	}
+	@Around("requestMapping()")
+	public Object aroundRequest(ProceedingJoinPoint pjp) throws Throwable {
+		return around(pjp);
+	}
+	
+	private final Object around(ProceedingJoinPoint pjp) throws Throwable {
+		logger.info("start @Arround - {} / {}", pjp.getSignature().getDeclaringTypeName(), pjp.getSignature().getName());
+        Object result = pjp.proceed();
+        logger.info("finished @Arround - {} / {}", pjp.getSignature().getDeclaringTypeName(), pjp.getSignature().getName());
+        return result;
+	}
+	
+	private final void before(JoinPoint pjp) {
+		logger.info("start @Before - {} / {}", pjp.getSignature().getDeclaringTypeName(), pjp.getSignature().getName());
+        logger.info("finished @Before - {} / {}", pjp.getSignature().getDeclaringTypeName(), pjp.getSignature().getName());
+	}
     
 }
