906805082f28ec3ec65c60fefbbc721baba1146e
ymkmoon <ymkmoon@naver.com>
2022-07-19 15:01:53 +0900

    Update to use one builder

--- src/main/java/com/toyseven/ymk/common/filter/OAuth2RequestFilter.java
+++ src/main/java/com/toyseven/ymk/common/filter/OAuth2RequestFilter.java
@@ -86,12 +86,14 @@ public class OAuth2RequestFilter extends OncePerRequestFilter {
     
     private void failResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
     	
-    	ErrorResponse fail = ErrorResponse.builder()
-				.status(errorCode.getHttpStatus().value())
-				.error(errorCode.getHttpStatus().name())
-                .code(errorCode.name())
-                .message(errorCode.getDetail())
-                .build();
+//    	ErrorResponse fail = ErrorResponse.builder()
+//				.status(errorCode.getHttpStatus().value())
+//				.error(errorCode.getHttpStatus().name())
+//                .code(errorCode.name())
+//                .message(errorCode.getDetail())
+//                .build();
+    	
+    	ErrorResponse fail = ErrorResponse.toBuilder(errorCode);
     	
 		response.setStatus(errorCode.getHttpStatus().value());
 	    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
