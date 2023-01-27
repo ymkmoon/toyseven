62d8805daeb15a9e555093d6a03057b9bccb83a6
ymkmoon <ymkmoon@naver.com>
2023-01-27 15:11:22 +0900

    style: Add log message in station job

--- src/main/java/com/toyseven/ymk/station/StationJob.java
+++ src/main/java/com/toyseven/ymk/station/StationJob.java
@@ -9,9 +9,11 @@ import com.toyseven.ymk.common.model.entity.StationInformationEntity;
 import com.toyseven.ymk.common.util.ToysevenCommonUtil;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class StationJob {
 	
 	private final StationService stationService;
@@ -19,10 +21,14 @@ public class StationJob {
 	@Scheduled(fixedDelay = 100000000)
     public void saveStations() {
         List<StationInformationEntity> stations = stationService.getStations();
+        log.info("==================== BEGIN Station Job ====================");
         if(!stations.isEmpty()) {
+        	log.info("Save data responsed from Third Party");
             stationService.saveStations(stations);
         } else {
+        	log.info("Save local data ");
         	stationService.saveStations(ToysevenCommonUtil.getStationsFromJsonArrayFile("StationFindAll.json"));
         }
+        log.info("==================== END Station Job ======================");
     }
 }
